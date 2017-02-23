d923a86384401f7f32becdd9352ac62d33ea9783
mahershammaa <amshammaa@mix.wvu.edu>
2017-02-23 15:39:29 -0500

    built multithreaded mesh writer

--- buildMeshes3.py
+++ buildMeshes3.py
@@ -9,15 +9,15 @@ def writeObj(filepath, vertices, normals, faces):
 	with open(filepath, 'w') as f:
 		f.write("# OBJ file\n")
 		for v in vertices:
-			f.write("v %.2f %.2f %.2f \n" % (v[0], v[1], v[2]*2))
+			f.write("v %.2f %.2f %.2f \n" % (v[0], v[1], v[2]))
 		for n in normals:
-			f.write("vn %.2f %.2f %.2f \n" % (-n[0], -n[1], -n[2]))
+			f.write("vn %.2f %.2f %.2f \n" % (n[0], n[1], n[2]))
 		for face in faces:
 			f.write("f %d %d %d \n" % (face[0]+1, face[1]+1, face[2]+1))
 
 
 def main():
-	labelsFolderPath = "out2/"
+	labelsFolderPath = "outMended/"
 	labelsPaths = sorted(glob.glob(labelsFolderPath +'*'))
 	labelStack = [tifffile.imread(labelsPaths[z]) for z in range(len(labelsPaths))]
 	labelStack = np.dstack(labelStack)
@@ -30,13 +30,13 @@ def main():
 		print(count)
 		count += 1
 		indices = np.where(labelStack==each)
-		blankImg = np.zeros(labelStack.shape, dtype=np.uint8)
-		blankImg[indices] = 1
+		print(len(indices[0]))
+		#code.interact(local=locals())
+		#blankImg = np.zeros(labelStack.shape, dtype=np.uint8)
+		#blankImg[indices] = 1
 		
-		vertices, normals, faces = march(blankImg.transpose(), 0)  # zero smoothing rounds
-		writeObj('meshes/' + str(each) + '.obj', vertices, normals, faces)
-
-		
+		#vertices, normals, faces = march(blankImg.transpose(), 1)  # zero smoothing rounds
+		#writeObj('meshes/' + str(each) + '.obj', vertices, normals, faces)
 		
 	
 

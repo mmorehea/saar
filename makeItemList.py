78e691061cae87c1284c64e57aad248232c6a843
Michael Morehead <mmorehea@mix.wvu.edu>
2017-06-15 16:29:18 -0400

    script to generate outfile

--- makeItemList.py
+++ makeItemList.py
@@ -6,7 +6,6 @@ import glob
 from numpy import load
 import multiprocessing
 from multiprocessing.dummy import Pool as ThreadPool
-import queue
 from timeit import default_timer as timer
 import threading
 import os
@@ -33,14 +32,65 @@ def findBBDimensions(listOfPixels):
 
 	return [minxs-2, maxxs+2, minys-2, maxys+2, minzs-2, maxzs+2], [dx, dy, dz]
 
+def trackSize(labelStack, axis, start):
+	tracker = {}
+	for i in xrange(labelStack.shape[axis]):
+		end = timer()
+		print(str(i) + "/" + str(labelStack.shape[axis]) + " time: " + str(end-start))
+
+		if axis == 0:
+			img = labelStack[i,:,:]
+		elif axis == 1:
+			img = labelStack[:,i,:]
+		elif axis == 2:
+			img = labelStack[:,:,i]
+
+		idList = np.unique(img)
+
+		for each in tracker.keys():
+			tracker[each][2] += 1
+			if tracker[each][2] > 25:
+				if tracker[each][1] - tracker[each][0] < 500:
+					del tracker[each]
+
+		for itm in idList:
+
+			if itm not in tracker.keys():
+				tracker[itm] = [i, 0, 0]
+			else:
+				if i > tracker[itm][1]:
+					tracker[itm][1] = i + 1
+					tracker[itm][2] = 0
+
+	finalList = [t for t in tracker.keys() if tracker[t][1] - tracker[t][0] > 500]
+	return finalList
+
 def main():
 	start = timer()
 	labelsFolderPath = sys.argv[1]
+	code.interact(local=locals())
 	labelsPaths = sorted(glob.glob(labelsFolderPath +'*'))
 	labelStack = [tifffile.imread(labelsPaths[z]) for z in range(len(labelsPaths))]
+	code.interact(local=locals())
 	labelStack = np.dstack(labelStack)
 	end = timer()
  	print("Loaded data... time: " + str(end-start))
+	code.interact(local=locals())
+	print("X Direction...")
+	finalListX = trackSize(labelStack, 0, start)
+	print("Y Direction...")
+	finalListY = trackSize(labelStack, 1, start)
+	print("Z Direction...")
+	finalListZ = trackSize(labelStack, 2, start)
+
+	finalList = list(set(finalListX) | set(finalListY) | set(finalListZ))
+	print(end-start)
+
+	code.interact(local=locals())
+	pickle.dump(finalList, open('outfile.p', 'wb'))
+
+if __name__ == "__main__":
+	main()
 
 	# idList = np.unique(labelStack)
 	# print("Loaded ID list...")
@@ -60,80 +110,74 @@ def main():
 	# code.interact(local=locals())
 	# pickle.dump(goodLabels, open('outfile.p', 'wb'))
 
-	print("X Direction...")
-	tracker = {}
-	for i in xrange(labelStack.shape[0]):
-		end = timer()
-		print(str(i) + "/" + str(labelStack.shape[0]) + " time: " + str(end-start))
-		imgx = labelStack[i,:,:]
-		idList = np.unique(imgx)
-
-		for each in tracker.keys():
-			tracker[each][2] += 1
-			if tracker[each][2] > 25:
-				del tracker[each]
-
-		for itm in idList:
-
-			if itm not in tracker.keys():
-				tracker[itm] = [i, 0, 0]
-			else:
-				if i > tracker[itm][1]:
-					tracker[itm][1] = i + 1
-					tracker[itm][2] = 0
-	finalListX = set([tracker[t] for t in tracker.keys() if tracker[t][1] - tracker[t][0] > 500])
-
-	print("Y Direction...")
-	tracker = {}
-	for i in xrange(labelStack.shape[1]):
-		end = timer()
-		print(str(i) + "/" + str(labelStack.shape[1]) + " time: " + str(end-start))
-		imgy = labelStack[:,i,:]
-		idList = np.unique(imgy)
-
-		for each in tracker.keys():
-			tracker[each][2] += 1
-			if tracker[each][2] > 25:
-				del tracker[each]
-
-		for itm in idList:
-
-			if itm not in tracker.keys():
-				tracker[itm] = [i, 0, 0]
-			else:
-				if i > tracker[itm][1]:
-					tracker[itm][1] = i + 1
-					tracker[itm][2] = 0
-	finalListY = set([tracker[t] for t in tracker.keys() if tracker[t][1] - tracker[t][0] > 500])
-
-	print("Z Direction...")
-	tracker = {}
-	for i in xrange(labelStack.shape[2]):
-		end = timer()
-		print(str(i) + "/" + str(labelStack.shape[2]) + " time: " + str(end-start))
-		imgz = labelStack[:,:,i]
-		idList = np.unique(imgz)
-
-		for each in tracker.keys():
-			tracker[each][2] += 1
-			if tracker[each][2] > 25:
-				del tracker[each]
-
-		for itm in idList:
-
-			if itm not in tracker.keys():
-				tracker[itm] = [i, 0, 0]
-			else:
-				if i > tracker[itm][1]:
-					tracker[itm][1] = i + 1
-					tracker[itm][2] = 0
-	finalListZ = set([tracker[t] for t in tracker.keys() if tracker[t][1] - tracker[t][0] > 500])
-
-	finalList = list(set(finalListX) | set(finalListY) | set(finalListZ))
-
-	code.interact(local=locals())
-	pickle.dump(finalList, open('outfile.p', 'wb'))
-
-
-if __name__ == "__main__":
-	main()
+	# print("X Direction...")
+	# tracker = {}
+	# for i in xrange(labelStack.shape[0]):
+	# 	end = timer()
+	# 	print(str(i) + "/" + str(labelStack.shape[0]) + " time: " + str(end-start))
+	# 	imgx = labelStack[i,:,:]
+	# 	idList = np.unique(imgx)
+	#
+	# 	for each in tracker.keys():
+	# 		tracker[each][2] += 1
+	# 		if tracker[each][2] > 25:
+	# 			if tracker[each][1] - tracker[each][0] < 500:
+	# 				del tracker[each]
+	#
+	# 	for itm in idList:
+	#
+	# 		if itm not in tracker.keys():
+	# 			tracker[itm] = [i, 0, 0]
+	# 		else:
+	# 			if i > tracker[itm][1]:
+	# 				tracker[itm][1] = i + 1
+	# 				tracker[itm][2] = 0
+	# finalListX = set([tracker[t] for t in tracker.keys() if tracker[t][1] - tracker[t][0] > 500])
+	#
+	# print("Y Direction...")
+	# tracker = {}
+	# for i in xrange(labelStack.shape[1]):
+	# 	end = timer()
+	# 	print(str(i) + "/" + str(labelStack.shape[1]) + " time: " + str(end-start))
+	# 	imgy = labelStack[:,i,:]
+	# 	idList = np.unique(imgy)
+	#
+	# 	for each in tracker.keys():
+	# 		tracker[each][2] += 1
+	# 		if tracker[each][2] > 25:
+	# 			if tracker[each][1] - tracker[each][0] < 500:
+	# 				del tracker[each]
+	#
+	# 	for itm in idList:
+	#
+	# 		if itm not in tracker.keys():
+	# 			tracker[itm] = [i, 0, 0]
+	# 		else:
+	# 			if i > tracker[itm][1]:
+	# 				tracker[itm][1] = i + 1
+	# 				tracker[itm][2] = 0
+	# finalListY = set([tracker[t] for t in tracker.keys() if tracker[t][1] - tracker[t][0] > 500])
+	#
+	# print("Z Direction...")
+	# tracker = {}
+	# for i in xrange(labelStack.shape[2]):
+	# 	end = timer()
+	# 	print(str(i) + "/" + str(labelStack.shape[2]) + " time: " + str(end-start))
+	# 	imgz = labelStack[:,:,i]
+	# 	idList = np.unique(imgz)
+	#
+	# 	for each in tracker.keys():
+	# 		tracker[each][2] += 1
+	# 		if tracker[each][2] > 25:
+	# 			if tracker[each][1] - tracker[each][0] < 500:
+	# 				del tracker[each]
+	#
+	# 	for itm in idList:
+	#
+	# 		if itm not in tracker.keys():
+	# 			tracker[itm] = [i, 0, 0]
+	# 		else:
+	# 			if i > tracker[itm][1]:
+	# 				tracker[itm][1] = i + 1
+	# 				tracker[itm][2] = 0
+	# finalListZ = set([tracker[t] for t in tracker.keys() if tracker[t][1] - tracker[t][0] > 500])

9e2965d724eed2d350f845c0dc14524f3e25c8b4
Michael Morehead <mmorehea@mix.wvu.edu>
2017-07-12 14:13:44 -0400

    working on combining all scripts into one

--- saar.py
+++ saar.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import cv2
 import numpy as np
 import os
@@ -6,132 +7,266 @@ import glob
 import code
 import tifffile
 from timeit import default_timer as timer
-
+import ConfigParser
 from scipy import ndimage as nd
-
 from skimage import measure
 try:
     from skimage import filters
 except ImportError:
     from skimage import filter as filters
+from itertools import cycle
+import multiprocessing
+from multiprocessing.dummy import Pool as ThreadPool
 
-import matplotlib.pyplot as plt
+# from mass.py
+NUMBERCORES = multiprocessing.cpu_count()
+print "Found " + str(NUMBERCORES) + " number of cores. Using " + str(NUMBERCORES - 1) + "."
+NUMBERCORES -= 1
+
+threshVal = 0
+p = 0
+lowerSizeVal = 0
+upperSizeVal = 0
+outDir = ''
+# -----
 
 
 def adjustThresh(originalImg, value):
 	ret,thresh1 = cv2.threshold(originalImg, int(value), 255, cv2.THRESH_BINARY)
-	kernel = np.ones((2,2),np.uint8)
-	thresh1 = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
-	thresh1 = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, kernel)
-
+	kernel = np.ones((3,3),np.uint8)
+	thresh1 = cv2.dilate(thresh1, kernel, 1)
+	thresh1 = np.uint8(nd.morphology.binary_fill_holes(thresh1))
+	ret,thresh1 = cv2.threshold(thresh1, 0, 255, cv2.THRESH_BINARY)
+	thresh1 = cv2.erode(thresh1, kernel, 1)
 	return thresh1
+
+def adjustContours(kernelImg, kernelSize):
+	# kernelImg = cv2.cvtColor(kernelImg, cv2.COLOR_BGR2GRAY)
+	# print kernelImg.dtype
+	blank = np.zeros(kernelImg.shape)
+
+	if cv2.__version__[0] == '3':
+		contourImage, contours, hierarchy = cv2.findContours(kernelImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE )
+	else:
+		contours, hierarchy = cv2.findContours(kernelImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE )
+	cv2.drawContours(blank, contours, -1, (255,255,255), -1)
+	kernel = np.ones((kernelSize,kernelSize),np.uint8)
+
+	#blank = cv2.dilate(blank, kernel, 1)
+	#blank = cv2.erode(blank, kernel, 2)
+	return np.uint8(blank)
 
 def nothing(x):
     pass
 
-def processEntireStack(path, threshValue):
-	emFolderPath = "cropedEM/"
-	emPaths = sorted(glob.glob(emFolderPath +'*'))
-	emImages = [cv2.imread(emPaths[z], -1) for z in xrange(len(emPaths))]
-	processedStack = []
-	for ii, each in enumerate(emImages):
-		print str(ii) + " / " + str(len(emImages))
-		threshImg = adjustThresh(each, threshValue)
-		processedImg = contourAndErode(each, threshImg)
-		processedStack.append(processedImg)
-
-	processedStack = np.dstack(processedStack)
-	return processedStack
-
-
-def contourAndErode(img, threshImg):
-	blank = np.zeros(img.shape)
-	kernel = np.ones((3,3),np.uint8)
-	if cv2.__version__[0] == '3':
-		contourImage, contours, hierarchy = cv2.findContours(threshImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE )
-	else:
-		contours, hierarchy = cv2.findContours(threshImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE )
-	cv2.drawContours(blank, contours, -1, (255,255,255), -1)
-	kernel = np.ones((2,2),np.uint8)
-	blank = cv2.morphologyEx(blank, cv2.MORPH_CLOSE, kernel)
-	kernel = np.ones((2,2),np.uint8)
-	blank = cv2.erode(blank, kernel, 1)
-	return blank
-
-
-
-def main():
-	
-
-	#emFolderPath = "cropedEM/"
-	#emPaths = sorted(glob.glob(emFolderPath +'*'))
-
-	#emImages = [cv2.imread(emPaths[z], -1) for z in xrange(len(emPaths))]
-	#emImages = np.dstack(emImages)
-	em = "cropedEM/Crop_mendedEM-0000.tiff"
-	img = cv2.imread(em, -1)
+def threshVis(img):
 	oldThresh = 200
 	cv2.namedWindow('image')
-	#code.interact(local=locals())
 
 	# create trackbars for picking threshold
 	cv2.createTrackbar('Threshold', 'image', 0, 255, nothing)
 	threshImg = img
 	while(1):
-		cv2.imshow('image', threshImg)
 		k = cv2.waitKey(1)
 		if k == 32:
 			break
+		try:
+			# threshImg = cv2.resize(threshImg, (1900, 1200))
+			cv2.imshow('image', threshImg)
+		except:
+			print 'WARNING: cv2 did not read the image correctly'
 
-
-			# get current positions of four trackbars
+		# get current positions of four trackbars
 		r = cv2.getTrackbarPos('Threshold','image')
 		if (r != oldThresh):
 			oldThresh = r
 			threshImg = adjustThresh(img, r)
 
+	cv2.destroyAllWindows()
+
+	return oldThresh, threshImg
+
+def noiseVis(threshImg):
+	oldKernel = 2
+	ks = oldKernel
+	cv2.namedWindow('image')
+	threshImg = np.uint8(threshImg)
+
+	kernelImg = np.uint8(threshImg)
+	cv2.createTrackbar('Kernel Size for Noise Removal', 'image', 1, 10, nothing)
+	while(1):
+		k = cv2.waitKey(1)
+		if k == 32:
+			break
+		try:
+			# kernelImg = cv2.resize(kernelImg, (1900, 1200))
+			cv2.imshow('image', kernelImg)
+		except:
+			print 'WARNING: cv2 did not read the image correctly'
+		ks = cv2.getTrackbarPos('Kernel Size for Noise Removal', 'image')
+		# get current positions of four trackbars
+
+		if (ks != oldKernel):
+			# print ks
+			oldKernel = ks
+			kernelImg = cv2.morphologyEx(threshImg, cv2.MORPH_OPEN, np.ones((ks,ks)))
+			ret,kernelImg = cv2.threshold(kernelImg, 0, 255, cv2.THRESH_BINARY)
+			# kernelImg = cv2.erode(kernelImg, (ks,ks), 6)
+
+	cv2.destroyAllWindows()
+	return oldKernel, kernelImg
+
+def sizeVis(img):
+
+	sizeRange = [0,99]
+	cv2.namedWindow('image')
+
+	cv2.createTrackbar('Lowest Size Percentile', 'image', 0, 100, nothing)
+	cv2.createTrackbar('Highest Size Percentile', 'image', 99, 100, nothing)
+	threshImg = img
+	while(1):
+		k = cv2.waitKey(1)
+		if k == 32:
+			break
+		try:
+			# threshImg = cv2.resize(threshImg, (1900, 1200))
+			cv2.imshow('image', threshImg)
+		except:
+			print 'WARNING: cv2 did not read the image correctly'
+
+		# get current positions of four trackbars
+		lowerPercentile = cv2.getTrackbarPos('Lowest Size Percentile','image')
+		higherPercentile = cv2.getTrackbarPos('Highest Size Percentile','image')
+
+		if (lowerPercentile != sizeRange[0] or higherPercentile != sizeRange[1]):
+			sizeRange[0] = lowerPercentile
+			sizeRange[1] = higherPercentile
+			threshImg = adjustSizeFilter(img, lowerPercentile, higherPercentile)
 
 
-	startMain = timer()
-	print "Contouring entire stack..."
-	blank = processEntireStack('ok', oldThresh)
-	endContourStack = timer() - startMain
+	cv2.destroyAllWindows()
+	return sizeRange, threshImg
+
+def findCentroid(listofpixels):
+	if len(listofpixels) == 0:
+		return (0,0)
+	rows = [p[0] for p in listofpixels]
+	cols = [p[1] for p in listofpixels]
+	try:
+		centroid = int(round(np.mean(rows))), int(round(np.mean(cols)))
+	except:
+		print 'error'
+		code.interact(local=locals())
+		centroid = (0,0)
+	return centroid
+
+def adjustSizeFilter(img, lowerPercentile, higherPercentile):
+	label_img, cc_num = nd.label(img)
+	objs = nd.find_objects(label_img)
+	areas = nd.sum(img, label_img, range(cc_num+1))
+
+	indices = sorted(range(len(areas)), key = lambda k: areas[k])
+
+	orderedAreas = [areas[ind] for ind in indices]
+
+	lowerThresh = orderedAreas[int((float(lowerPercentile)/100) * len(orderedAreas))]
+	if higherPercentile != 100:
+		upperThresh = orderedAreas[int((float(higherPercentile)/100) * len(orderedAreas))]
+	else:
+		upperThresh = orderedAreas[-1]
+
+	area_mask = (areas < lowerThresh)
+	label_img[area_mask[label_img]] = 0
+
+	area_mask = (areas > upperThresh)
+	label_img[area_mask[label_img]] = 0
+
+	# print np.ndarray.dtype(label_img)
+	label_img[np.where(label_img > 0)] = 2**16
+
+	return label_img
+
+def getParameters(img):
+	oldThresh, threshImg = threshVis(img)
+	noiseKernel, threshImg = noiseVis(threshImg)
+	sizeRange, threshImg = sizeVis(threshImg)
+
+	print "Writing configuration file..."
+	cfgfile = open("saar.ini",'w')
+	Config = ConfigParser.ConfigParser()
+	Config.add_section('Options')
+	Config.set('Options','Threshold Value', oldThresh)
+	Config.set('Options','Remove Noise Kernel Size', noiseKernel)
+	Config.set('Options','Filter Size Lower Bound', sizeRange[0])
+	Config.set('Options','Filter Size Upper Bound', sizeRange[1])
+	Config.write(cfgfile)
+	cfgfile.close()
+
+def generateMeshes(emImages):
+	cfgfile = open("saar.ini",'r')
+	config = ConfigParser.ConfigParser()
+	config.read('saar.ini')
+
+	try:
+		global threshVal
+		threshVal = int(config.get('Options', 'Threshold Value'))
+	except:
+		print "threshVal not found in config file, did you set the parameters?"
+	try:
+		global p
+		p = int(config.get('Options', 'Remove Noise Kernel Size'))
+	except:
+		print "kernel value not found in config file, did you set the parameters?"
+	try:
+		global lowerSizeVal
+		lowerSizeVal = int(config.get('Options', 'Filter Size Lower Bound'))
+		global upperSizeVal
+		upperSizeVal = int(config.get('Options', 'Filter Size Upper Bound'))
+	except:
+		print "size values not found in config file, did you set the parameters?"
+
+	pool = ThreadPool(NUMBERCORES)
+
+	for i, _ in enumerate(pool.imap_unordered(processSlice, emImages), 1):
+	    sys.stderr.write('\rdone {0:%}'.format(i/len(emImages)))
+
+	# processedStack = pool.map(processSlice, images)
+
+	print "time, mass, single: " + str(timer() - start)
+
+def main():
 	start = timer()
-	print "Finding connection..."
-	labels = nd.measurements.label(blank)
-	labels = labels[0]
-	endConnections = timer() - start
-	
-	print "Writing file..."
-	start = timer()
-	for each in range(labels.shape[2]):
-		print each
-		img = labels[:,:,each]
-		tifffile.imsave("out/" + str(each) + '.tif', img)
-	endWritingFile = timer() - start
+	emFolderPath = sys.argv[1]
+	emPaths = sorted(glob.glob(emFolderPath +'*.tif*'))
+	emImages = [cv2.imread(path,-1) for path in emPaths]
 
-	endTime = timer()
+	em = emPaths[0]
+	img = cv2.imread(em, 0)
+	img = np.uint8(img)
 
-	with open('runStats.txt', 'w') as f:
-		f.write('Run Stats \n')
-		f.write('Run start: ' + str(startMain) + '\n')
-		f.write('Total time: '+ str(endTime - startMain) + '\n')
-		f.write('Contour time: ' + str(endContourStack) + '\n')
-		f.write('Connection time: ' + str(endConnections) + '\n')
-		f.write('Writing file time: ' + str(endWritingFile) + '\n')
-		f.write('Total number of labels: ' + str(np.unique(labels)))
-
-
-
-
-
-
-
+	while True:
+		print("SAAR MENU")
+		print("1. Set Parameters")
+		print("2. Generate Meshes (takes about a day)")
+		print("3. Separete False Merges")
+		print("4. Quit")
+		choice = raw_input(">")
+		if choice=='1':
+			getParameters(img)
+			code.interact(local=locals())
+			oldThresh, threshImg = threshVis(img)
+		elif choice=='2':
+			generateMeshes(emImages)
+			noiseKernel, threshImg = noiseVis(threshImg)
+		elif choice=='3':
+			sizeRange, threshImg = sizeVis(threshImg)
+		elif choice=='4':
+			sys.exit()
+		else:
+			continue
 
 
 
-
-cv2.destroyAllWindows()
 
 if __name__ == "__main__":
 	main()

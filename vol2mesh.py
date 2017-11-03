34320532e04efbfe0781753b4e3777728337f28b
Michael Morehead <mmorehea@mix.wvu.edu>
2017-11-03 13:04:21 -0400

    done!

--- vol2mesh.py
+++ vol2mesh.py
@@ -223,6 +223,9 @@ def main():
 		except:
 			code.interact(local=locals())
 		print(box)
+		if dimensions[0] > 500 and dimensions[1] > 500 and dimensions[2] > 500:
+			print('skipped')
+			continue
 
 
 		window = labelStack[box[0]:box[1], box[2]:box[3], box[4]:box[5]]

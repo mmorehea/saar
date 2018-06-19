7d18d254e6ebb33357a0f42754d5b14ca46bbad8
Michael Morehead <mmorehea@mix.wvu.edu>
2018-06-19 15:21:18 -0400

    a

--- extractLabels.py
+++ extractLabels.py
@@ -6,7 +6,7 @@ import glob
 import code
 import tifffile
 from timeit import default_timer as timer
-import ConfigParser
+#import ConfigParser
 
 from scipy import ndimage as nd
 
@@ -22,22 +22,24 @@ def main():
 	threshPath = sys.argv[1]
 	threshPaths = sorted(glob.glob(threshPath +'*'))
 	
-	emImages = [cv2.imread(threshPaths[z], -1) for z in xrange(len(threshPaths))]
+	emImages = [tifffile.imread(threshPaths[z]) for z in range(len(threshPaths))]
+	code.interact(local=locals())
 	images = np.dstack(emImages)
-	blank = np.zeros(images.shape)
-	labelList = [2690, 24278]	
+	blank = np.zeros(images.shape, dtype=np.uint32)
+	labelList = [150391,202863,393889,1397,2334333,93924,261413]	
 	for each in labelList:
 		indices = np.where(images == each)
 		blank[indices] = each
 		
-	blank = np.uint16(blank)
 	
-	for each in xrange(blank.shape[2]):
-		print each
+	
+	for each in range(blank.shape[2]):
+		print(each)
 		img = blank[:,:,each]
+		code.interact(local=locals())
 		tifffile.imsave('extractedLabels/' + str(each).zfill(4) + '.tif', img)
 	endClean = timer() - startMain
-	print "time, labels, single: " + str(endClean)
+	print("time, labels, single: " + str(endClean))
 
 
 
